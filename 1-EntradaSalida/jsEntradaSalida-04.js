6758bb6936bb0cc80707bfb7035f2890ea4d3d0d
AtomicDuck870 <Erikcorimayo@hotmail.com>
2023-02-01 11:51:54 -0300

    realize el ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,15 @@
-/* 
+/* erik corimayo
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let datoIngresado;
+
+	datoIngresado = prompt("Ingrese su nombre");
+	document.getElementById("txtIdNombre").value=datoIngresado
+
+
+	alert("Nombre ingresado con exito");
 }
 
