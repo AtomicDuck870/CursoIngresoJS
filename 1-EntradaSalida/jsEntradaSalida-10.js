a84578a8112721b11d8d9882159b8724773c2c3e
AtomicDuck870 <Erikcorimayo@hotmail.com>
2023-02-01 21:43:40 -0300

    ejercicio 10 hecho

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,10 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let numUnoText;
+	let numUno;
+	
+	numUnoText=document.getElementById("txtIdImporte").value;
+	numUno=parseInt(numUnoText);
+	document.getElementById("txtIdResultado").value=Math.floor(numUno-numUno*25/100);
 }
