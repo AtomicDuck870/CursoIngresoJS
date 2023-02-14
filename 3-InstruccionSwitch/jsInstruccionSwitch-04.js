a22980981ed23868b2bfdf67e18afd8690a3e2e0
AtomicDuck870 <Erikcorimayo@hotmail.com>
2023-02-13 22:29:51 -0300

    if 1 restored, switch 1, 4 y 5

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,5 +1,50 @@
 function mostrar()
 {
+	let mes;
+	let mensaje;
+
+	mes = document.getElementById("txtIdMes").value;
+
+	switch(mes)
+	{
+		case "Enero":
+			mensaje = "Este mes tiene 31 dias.";
+			break;
+		case "Febrero":
+			mensaje = "Este mes tiene 28 dias.";
+			break;
+		case "Marzo":
+			mensaje = "Este mes tiene 31 dias.";
+			break;
+		case "Abril":
+			mensaje = "Este mes tiene 30 dias.";
+			break;
+		case "Mayo":
+			mensaje = "Este mes tiene 31 dia.";
+			break;
+		case "Junio":
+			mensaje = "Este mes tiene 30 dias.";
+			break;
+		case "Julio":
+			mensaje = "Este mes tiene 31 dias.";
+			break;
+		case "Agosto":
+			mensaje = "Este mes tiene 31 dias.";
+			break;
+		case "Septiembre":
+			mensaje = "Este mes tiene 30 dias.";
+			break;
+		case "Octubre":
+			mensaje = "Este mes tiene 31 dias.";
+			break;
+		case "Noviembre":
+			mensaje = "Este mes tiene 30 dias.";
+			break;
+		case "Diciembre":
+			mensaje = "Este mes tiene 31 dias.";
+			break;
+	}
+	alert(mensaje);
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
 	alert(mesDelAño);
--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,5 +1,17 @@
 function mostrar()
 {
+	let horaIngresada;
+	let mensaje;
+
+	horaIngresada = parseInt(document.getElementById("txtIdHora").value);
+
+	switch(horaIngresada)
+	{
+		case "7:00":
+			mensaje = horaIngresada + " Es de mañana.";
+			break;
+	}
+	alert(mensaje);
 	//tomo la hora
 	var horaDelDia =txtIdHora.value;
 	alert(horaDelDia);
