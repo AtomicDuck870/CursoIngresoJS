4597dd5b404d5ec086bacb0fae4e2830e4f7585c
AtomicDuck870 <Erikcorimayo@hotmail.com>
2023-02-14 17:58:52 -0300

    TP4 hecho sin reglas.

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,4 +1,5 @@
-/*4.	Para el departamento de iluminación:
+/*Erik corimayo
+4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
@@ -10,24 +11,74 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	let cantidadLamparas;
+    let precioLampara;
+    let cantidadLamparas;
     let marca;
     let precioDescuento;
-    let precioLampara;
-    let precioSinDescuento;
+    let porcentaje;
+    let impuesto;
 
+    precioLampara = 35;
     cantidadLamparas = parseInt(document.getElementById("txtIdCantidad").value);
     marca = document.getElementById("Marca").value;
-    precioLampara = 35;
-    precioSinDescuento = cantidadLamparas * precioLampara;
 
-    if(cantidadLamparas >= 6)
+    switch(cantidadLamparas)
     {
-        precioDescuento = precioSinDescuento - precioSinDescuento * (50 / 100);
-        document.getElementById("txtIdprecioDescuento").value = precioDescuento + " (50% de descuento)";
-    }else{if(cantidadLamparas == 5 && marca == "ArgentinaLuz")
+        case 1:
+        case 2:
+            porcentaje = 0;
+            break;
+        case 3:
+            switch(marca)
+            {
+                case "ArgentinaLuz":
+                    porcentaje = 15;
+                    break;
+                case "FelipeLamparas":
+                    porcentaje = 10;
+                    break;
+                default:
+                    porcentaje = 5;
+                    break;
+            }
+            break;
+        case 4:
+            switch(marca)
+            {
+                case "ArgentinaLuz":
+                case "FelipeLamparas":
+                    porcentaje = 25;
+                    break;
+                default:
+                    porcentaje = 20;
+                    break;
+            }
+            break;
+        case 5:
+            switch(marca)
+            {
+                case "ArgentinaLuz":
+                    porcentaje = 40;
+                    break;
+                default:
+                    porcentaje = 30;
+                    break;
+            }
+            break;
+        default:
+            porcentaje = 50;
+            break;
+    }
+
+    precioDescuento = precioLampara*cantidadLamparas;
+    precioDescuento = precioDescuento - precioDescuento*porcentaje/100;
+
+    if(precioDescuento >= 120)
     {
-        
+        impuesto = precioDescuento*10/100;
+        alert("Usted pago "+impuesto+" de IIBB.");
+        precioDescuento = precioDescuento + impuesto;
     }
-    }
+
+    document.getElementById("txtIdprecioDescuento").value = precioDescuento;
 }
