00744eafaff254c92d2c5ab51c62f9ffd169e42f
AtomicDuck870 <Erikcorimayo@hotmail.com>
2023-02-28 08:51:39 -0300

    for 8

--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -3,42 +3,28 @@ Enunciado:
 al presionar el botón pedir un número. Informar si el numero es PRIMO o no.*/
 function mostrar()
 {
-	/*let numero;
+	let numero;
 	let contadorD;
+	let respuesta;
 
 	contadorD = 0;
 
 	numero = parseInt(prompt("Ingrese un numero"));
 
-	for(let i=2; i<numero; i++)
+	for(let i=0; i<=numero; i++)
 	{
-		if(numero%i == 0)
-	}
-	alert("ok");*/
-
-	let numero;
-	let primo;
-	let noPrimo;
-	let mensaje;
-	let contador;
-
-	numero = parseInt(prompt("Ingrese un numero"));
-	if(numero == 0 || numero == 1 || numero == 4)
-	for(i=1; i<numero / 2; i++)
-	{
-		if(numero%i == 0)
+		if(numero % i == 0)
 		{
-			primo = numero;
-			mensaje = "este numero es primo: "+primo;
-			contador = contador+2;
-		}
-		else
-		{
-			noPrimo = numero;
-			mensaje = "este numero NO es primo: "+noPrimo;
-			contador = contador+2;
+			contadorD = contadorD+1;
+			if(contadorD == 2)
+			{
+				respuesta = numero+" es primo";
+			}
+			else
+			{
+				respuesta = numero+" no es primo";
+			}
 		}
 	}
-	console.log(mensaje);
-
+	console.log(respuesta);
 }//FIN DE LA FUNCIÓN
